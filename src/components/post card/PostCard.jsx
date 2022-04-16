82cf4d51fe2fc87ccb6812515f6a51aef4607661
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-04-17 02:51:29 +0530

    styling:adjusted card width and its content

--- src/components/post card/PostCard.jsx
+++ src/components/post card/PostCard.jsx
@@ -1,13 +1,14 @@
-import classes from './postCard.module.css'
+import classes from "./postCard.module.css";
 
-const PostCard = ({post}) => {
+const PostCard = ({ post }) => {
   return (
     <div className={classes["post-container"]}>
-      <div className="avatar avatar-text"><span>{post.username.substring(0,2).toUpperCase()}</span></div>
+      <div className="avatar avatar-text">
+        <span>{post.username.substring(0, 2).toUpperCase()}</span>
+      </div>
       <div className={classes["post-body-container"]}>
         <div className={classes["username-id-container"]}>
           <div className={classes["username-body"]}>
-            {/* <span className="text-white"></span> */}
             <span className="text-white">@{post.username}</span>
             <span className="text-white">3h</span>
           </div>
@@ -16,9 +17,7 @@ const PostCard = ({post}) => {
           </span>
         </div>
         <div className="tweet-body">
-          <p className="text-white">
-            {post.content}
-          </p>
+          <p className="text-white">{post.content}</p>
         </div>
         <div className={classes["post-footer"]}>
           <span>
