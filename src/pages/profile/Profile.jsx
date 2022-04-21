3467e0055ac80d739f801b365d969211b69725e5
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-04-22 01:26:00 +0530

    feat : profile screen

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,12 +1,13 @@
 import classes from "./profile.module.css";
 import { useEffect, useState, useRef } from "react";
-import {  useUser } from "../../context";
-import {ProfileCard} from '../../components';
+import { usePost, useUser } from "../../context";
+import { ProfileCard,PostCard } from "../../components";
 
 const Profile = () => {
   const { getUser } = useUser();
   const [user, setUser] = useState(null);
   const mountedRef = useRef(false);
+  const {postState} = usePost()
 
   useEffect(() => {
     mountedRef.current = true;
@@ -20,12 +21,20 @@ const Profile = () => {
     fetchUser();
 
     return () => (mountedRef.current = false);
-  }, []);
+  },[postState]);
 
   return (
     <main className={classes["profile-container"]}>
       <h1 className="centered-text text-primary">Profile</h1>
-      {user ? <ProfileCard user={user}/> : <h1 className="text-primary">Loading...</h1>}
+      {!user && <h1 className="text-primary">Loading...</h1>}
+      {user && <ProfileCard user={user} />}
+      {user && (
+        <div className={classes["post-list"]}>
+          {postState.posts.filter(post=>post.username===user.username).map((post) => {
+            return <PostCard key={post._id} post={post} />;
+          })}
+        </div>
+      )}
     </main>
   );
 };
