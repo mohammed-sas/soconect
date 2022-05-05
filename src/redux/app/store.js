2654a8bba913e39f67835454a52ff0f1135087ca
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-05-05 15:28:39 +0530

    config: redux store and posts reducer

---
+++ src/redux/app/store.js
@@ -0,0 +1,7 @@
+import {configureStore} from '@reduxjs/toolkit';
+import postsReducer from '../slices/postSlice';
+export const store =configureStore({
+    reducer:{
+        posts:postsReducer
+    }
+});
