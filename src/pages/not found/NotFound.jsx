d784d13c89760522162b3e8b90634c161aa3494a
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-05-21 13:53:21 +0530

    feat:added 404 page

---
+++ src/pages/not found/NotFound.jsx
@@ -0,0 +1,11 @@
+import classes from './notFound.module.css';
+
+const NotFound = () => {
+    return (
+        <div className={classes["container"]}>
+           <h2 className="centered-text">Error:404 Page Not Found!</h2> 
+        </div>
+    )
+}
+
+export default NotFound
